942d84c0e830ede2b6769341ac2501c80df1adc0
morg-ma <hodapop193@gmail.com>
2023-03-21 18:07:24 -0700

    Print Alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,13 @@
 #include "main.h"
+/*
+ * function print_alphabet : for print alphabets
+ */
 
 void print_alphabet(void)
 {
-        int i;
-        for (i = 97; i <= 122; i++)
-                _putchar(i);
-        _putchar('\n');
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
 }
