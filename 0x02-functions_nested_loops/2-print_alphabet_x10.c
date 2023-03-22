e21ff616394ab6bbc3d21c50281c91f32e0e282e
morg-ma <hodapop193@gmail.com>
2023-03-21 18:22:18 -0700

    Print Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print all alphabet in lowercase 10 times
+*/
+
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+		for (i = 97; i <= 122; i++)
+			_putchar(i);
+	_putchar('\n');
+}
