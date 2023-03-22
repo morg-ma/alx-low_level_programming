01fb139c59b923ec858971c24cfee9a22f6c82d1
morg-ma <hodapop193@gmail.com>
2023-03-21 17:37:04 -0700

    _Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch[] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	_putchar(ch[i]);
+
+	_putchar('\n');
+	return (0);
+}
