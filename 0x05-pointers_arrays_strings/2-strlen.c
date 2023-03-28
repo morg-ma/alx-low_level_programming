730ce1b885a6550b59a8a4042d81a3680034b0d8
morg-ma <hodapop193@gmail.com>
2023-03-27 19:01:29 -0700

    Create "C - pointers, arrays, strings"

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strlen - returns the length of a string
+* @s: string
+* Return: length
+*/
+int _strlen(char *s)
+{
+	int len,i;
+
+	for(i = 0; *s != '\0'; i++)
+	{
+		len++;
+	}
+	return len;
+}
