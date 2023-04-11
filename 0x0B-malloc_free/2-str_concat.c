44c820ee00e21d01ad47936221c052615e6068df
morg-ma <hodapop193@gmail.com>
2023-04-11 07:13:11 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	for (; s[n] != '\0'; n++)
+		;
+	return (n);
+}
+
+/**
+ * *str_concat - concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int n1, n2, i;
+	char *m;
+
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
+
+	n1 = _strlen(s1);
+	n2 = _strlen(s2);
+	m = malloc((n1 + n2) * sizeof(char) + 1);
+	if (m == 0)
+		return (0);
+
+	for (i = 0; i <= n1 + n2; i++)
+	{
+		if (i < n1)
+			m[i] = s1[i];
+		else
+			m[i] = s2[i - n1];
+	}
+	m[i] = '\0';
+	return (m);
+}
